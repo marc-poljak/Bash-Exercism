ff0fd4904d17d6c838fe10eda4ef2e16d165d276
Marc Poljak <marc.poljak@di-unternehmer.com>
2024-05-13 12:41:56 +0200

    Updated Armstrong Numbers

--- armstrong-numbers/armstrong_numbers.sh
+++ armstrong-numbers/armstrong_numbers.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env bash
 
 main () {
-    local num=$1
-    local original_num=$num
+    local num="$1"
+    local original_num="$num"
     local sum=0
     local digits=0
 
-    while [ $num -gt 0 ]; do
+    while [ "$num" -gt 0 ]; do
         digits=$((digits + 1))
         num=$((num / 10))
     done
 
-    num=$original_num
-    while [ $num -gt 0 ]; do
+    num="$original_num"
+    while [ "$num" -gt 0 ]; do
         local digit=$((num % 10))
         sum=$((sum + digit**digits))
         num=$((num / 10))
     done
 
-    if [ $sum -eq $original_num ]; then
+    if [ "$sum" -eq "$original_num" ]; then
         echo "true"
     else
         echo "false"
